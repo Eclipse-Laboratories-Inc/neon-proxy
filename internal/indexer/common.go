3693481b928c3bc05452d36bba439f23f3ef0b85
Argishti Malinyan <a.malinyan@neonlabs.org>
2023-04-04 17:55:37 +0400

    implemented indexed objects

--- internal/indexer/common.go
+++ internal/indexer/common.go
@@ -7,6 +7,116 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+type SolTxSigSlotInfo struct {
+	SolSign   string
+	BlockSlot int
+
+	hash int
+	str  string
+}
+
+type SolTxMetaInfo struct {
+	ident SolTxSigSlotInfo
+
+	blockSlot int
+	tx        map[string]string
+
+	str   string
+	reqID string
+}
+
+type Status int
+
+const (
+	UnknownStatus Status = iota
+	Success
+	Failed
+)
+
+type SolIxLogState struct {
+	program string
+	level   int
+
+	maxBpfCycleCnt  int
+	usedBpfCycleCnt int
+	usedHeapSize    int
+
+	logs      []SolIxLogState
+	innerLogs []SolIxLogState
+}
+
+type SolIxMetaInfo struct {
+	ix       map[string]string
+	idx      int
+	innerIdx int
+
+	program string
+	level   int
+	status  Status
+	err     error
+
+	usedHeapSize    int
+	maxBpfCycleCnt  int
+	usedBpfCycleCnt int
+
+	neonTxSig        string
+	neonGasUsed      int
+	neonTotalGasUsed int
+
+	neonTxReturn NeonLogTxReturn
+	neonTxEvents []NeonLogTxEvent
+}
+
+type SolTxCostInfo struct {
+	solSign   string
+	blockSlot int
+	operator  string
+	solSpent  int
+
+	str            string
+	calculatedStat bool
+}
+
+type SolTxLogDecoder struct{} //todo move to decoder?
+type NeonLogTxReturn struct{} // todo move to decoder?
+
+type Ident struct {
+	solSign   string
+	blockSlot int
+	idx       int
+	innerIdx  int
+}
+
+type SolNeonIxReceiptInfo struct {
+	metaInfo SolIxMetaInfo
+
+	solSign   string
+	blockSlot int
+
+	programIx int
+	ixData    []byte
+
+	neonStepCnt int
+
+	solTxCost SolTxCostInfo
+
+	ident Ident
+
+	str         string
+	accounts    []int
+	accountKeys []string
+}
+
+type SolTxReceiptInfo struct {
+	solCost  SolTxCostInfo
+	operator string
+
+	ixList         []map[string]string
+	innerIxList    []map[string]string
+	accountKeyList []string
+	ixLogMsgList   []SolIxLogState
+}
+
 func InsertBatchImpl(indexerDB DBInterface, pCounter prometheus.Counter, data []map[string]string) (int64, error) {
 	colums := indexerDB.GetColums()
 	sqlStr := fmt.Sprintf("INSERT INTO %s(%s) VALUES ", indexerDB.GetTableName(), strings.Join(colums, ", "))
--- internal/indexer/indexed_objects.go
+++ internal/indexer/indexed_objects.go
@@ -1,6 +1,244 @@
 package indexer
 
-type IndexedObject interface {
-	StartBlockSlot()
-	LastBlockSlot()
+import (
+	"strings"
+	"time"
+)
+
+type IndexedObjectInfo interface {
+	StartBlockSlot() int
+	LastBlockSlot() int
+}
+
+type NeonIndexedHolderInfo struct {
+	startBlockSlot int
+	lastBlockSlot  int
+}
+
+func (n NeonIndexedHolderInfo) StartBlockSlot() int {
+	return n.startBlockSlot
+}
+
+func (n NeonIndexedHolderInfo) LastBlockSlot() int {
+	return n.lastBlockSlot
+}
+
+func (n *NeonIndexedHolderInfo) AddSolanaNeonIx(solanaNeonIx SolNeonIxReceiptInfo) {
+	n.SetStartBlockSlot(solanaNeonIx.blockSlot)
+	n.SetLastBlockSlot(solanaNeonIx.blockSlot)
+}
+
+func (n *NeonIndexedHolderInfo) SetStartBlockSlot(blockSlot int) {
+	if n.startBlockSlot == 0 || blockSlot < n.startBlockSlot {
+		n.startBlockSlot = blockSlot
+	}
+}
+
+func (n *NeonIndexedHolderInfo) SetLastBlockSlot(blockSlot int) {
+	if blockSlot > n.lastBlockSlot {
+		n.lastBlockSlot = blockSlot
+	}
+}
+
+type TxStatus int
+
+const (
+	InProgress TxStatus = iota
+	Canceled
+	Done
+)
+
+type TxType int
+
+const (
+	UnknownType TxType = iota
+	Single
+	SingleFromAccount
+	IterFromData
+	IterFromAccount
+	IterFromAccountWoChainId
+)
+
+type TxInfoKey struct {
+	value string
+}
+
+func NewTxInfoKey(solNeonIx SolNeonIxReceiptInfo) TxInfoKey {
+	sign := solNeonIx.metaInfo.neonTxSig
+	if sign[:2] == "0x" {
+		sign = sign[2:]
+	}
+	return TxInfoKey{value: strings.ToLower(sign)}
+}
+
+func (ti *TxInfoKey) String() string {
+	return ti.value
+}
+
+func (ti *TxInfoKey) IsEmpty() bool {
+	return ti.value == ""
+}
+
+type NeonIndexedTxInfo struct {
+	startBlockSlot int
+	lastBlockSlot  int
+
+	key             TxInfoKey
+	neonReceipt     NeonTxReceiptInfo
+	txType          TxType
+	storageAccount  string
+	blockedAccounts []string
+	status          TxStatus
+	canceled        bool
+	neonEvents      []NeonLogTxEvent
+}
+
+func (n NeonIndexedTxInfo) StartBlockSlot() int {
+	return n.startBlockSlot
+}
+
+func (n NeonIndexedTxInfo) LastBlockSlot() int {
+	return n.lastBlockSlot
+}
+
+type NeonTxReceiptInfo struct {
+	neonTx    NeonTxInfo
+	neonTxRes NeonTxResultInfo
+}
+
+type NeonTxInfo struct {
+	addr     string
+	sig      string
+	nonce    string
+	gasPrice string
+	gasLimit string
+	toAddr   string
+	contract string
+	value    string
+	callData string
+	v        string
+	r        string
+	s        string
+	err      error
+}
+
+type NeonTxResultInfo struct {
+	blockSlot int
+	blockHash string
+	txIdx     int
+
+	solSig        string
+	solIxIdx      int
+	solIxInnerIdx int
+
+	neonSig string
+	gasUsed string
+	status  string
+
+	logs []map[string]string
+
+	canceledStatus int
+	lostStatus     int
+}
+
+type NeonAccountInfo struct {
+	neonAddress string
+	pdaAddress  string
+	blockSlot   int
+	code        string
+	solSig      string
+}
+
+type LogTxEventType int
+
+const (
+	Log LogTxEventType = 1
+
+	EnterCall         LogTxEventType = 101
+	EnterCallCode     LogTxEventType = 102
+	EnterStaticCall   LogTxEventType = 103
+	EnterDelegateCall LogTxEventType = 104
+	EnterCreate       LogTxEventType = 105
+	EnterCreate2      LogTxEventType = 106
+
+	ExitStop         LogTxEventType = 201
+	ExitReturn       LogTxEventType = 202
+	ExitSelfDestruct LogTxEventType = 203
+	ExitRevert       LogTxEventType = 204
+
+	Return LogTxEventType = 300
+	Cancel LogTxEventType = 301
+)
+
+type NeonLogTxEvent struct {
+	eventType LogTxEventType
+	Hidden    bool
+
+	address string
+	topics  []string
+	data    string
+
+	solSig       string
+	idx          int
+	innerIdx     int
+	totalGasUsed int
+	reverted     bool
+	eventLevel   int
+	eventOrder   int
+}
+
+type NeonIndexedBlockInfo struct {
+	solBlocks         []SolBlockInfo
+	historyBlockDeque []SolBlockInfo
+	completed         bool
+
+	neonHolders map[string]NeonIndexedHolderInfo
+	neonTxs     map[string]NeonIndexedTxInfo
+
+	doneNeonTxs []NeonIndexedTxInfo
+
+	solNeonIxs []SolNeonIxReceiptInfo
+	solTxCosts []SolTxCostInfo
+
+	StatNeonTxs map[TxType]NeonTxStatData
+}
+
+type SolBlockInfo struct{}       // todo implemented
+type NeonTxStatData struct{}     // todo implemented
+type SolTxMetaCollector struct{} // todo implemented
+
+type IndexedBlockStat struct {
+	neonBlockCnt    int
+	neonHolderCnt   int
+	neonTxCnt       int
+	historyBlockCnt int
+	solNeonIxCnt    int
+	minBlockSlot    int
+}
+
+type NeonIndexedBlockMap struct {
+	neonBlockMap       map[int]NeonIndexedBlockInfo
+	finalizedNeonBlock NeonIndexedBlockInfo
+	stat               IndexedBlockStat
+}
+
+type NeonIndexedBlockData struct {
+	neonIndexedBlockInfo NeonIndexedBlockInfo
+	finalized            bool
+}
+
+type SolNeonTxDecoderState struct {
+	startTime          time.Time
+	initBlockSlot      int
+	startBlockSlot     int
+	stopBlockSlot      int
+	solTxMetaCnt       int
+	solNeonIxCnt       int
+	solTxMetaCollector SolTxMetaCollector
+
+	solTx     SolTxReceiptInfo
+	solTxMeta SolTxMetaInfo
+	solNeonIx SolNeonIxReceiptInfo
+
+	neonBlockDeque []NeonIndexedBlockData
 }
