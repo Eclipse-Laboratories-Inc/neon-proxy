bd50a39aad4b8a3854c3511eb1c71970c7a14e83
Rigidus <i.am.rigidus@gmail.com>
2023-04-05 08:58:55 +0300

    websocket filters works

--- internal/wssubscriber/server/logs.go
+++ internal/wssubscriber/server/logs.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/neonlabsorg/neon-proxy/internal/wssubscriber/source"
+	"regexp"
 
 	"github.com/neonlabsorg/neon-proxy/internal/wssubscriber/utils"
 )
@@ -20,14 +21,24 @@ func (c *Client) subscribeToNewLogs(requestRPC SubscribeJsonRPC, responseRPC *Su
 		return
 	}
 
+	// check if user set up filters
 	if len(requestRPC.Params) > 1 {
-		params, ok := requestRPC.Params[1].(SubscribeLogsFilterParams)
-		if !ok {
-			responseRPC.Error = "newLogs subscription accepts filters only bu addresses and topics"
+		jsonParams, err := json.Marshal(requestRPC.Params[1])
+		if err != nil {
+			responseRPC.Error = "failed to read log filters"
 			return
 		}
 
-		c.buildFilters(params)
+		var filterParams SubscribeLogsFilterParams
+		if err := json.Unmarshal(jsonParams, &filterParams); err != nil {
+			responseRPC.Error = "failed to read log filters"
+			return
+		}
+
+		if err := c.buildFilters(filterParams); err != nil {
+			responseRPC.Error = fmt.Sprintf("failed to set up log filters: %v", err)
+			return
+		}
 	}
 
 	// if not subscribe to broadcaster
@@ -44,19 +55,32 @@ func (c *Client) subscribeToNewLogs(requestRPC SubscribeJsonRPC, responseRPC *Su
 	go c.CollectNewLogs()
 }
 
-func (c *Client) buildFilters(params SubscribeLogsFilterParams) {
+func isHexNumber(str string) bool {
+	hexPattern := "^0[xX][0-9a-fA-F]*$"
+	regex := regexp.MustCompile(hexPattern)
+	return regex.MatchString(str)
+}
+
+func (c *Client) buildFilters(params SubscribeLogsFilterParams) error {
 	c.logsFilters = &logsFilters{
 		Addresses: make(map[string]struct{}),
 		Topics:    make(map[string]struct{}),
 	}
 
 	for i := range params.Addresses {
+		// it seems, if field "address" in log is empty, than we return "address":"0x", but user may not know it
+		// and set up filter to get logs with empty "address" field as "" or "null"
+		// if "address" isn't "" or "null", it should be presented at least as hex number
+		if params.Addresses[i] != "null" && params.Addresses[i] != "" && !isHexNumber(params.Addresses[i]) {
+			return fmt.Errorf("can't filter by %v addres, addres should be hex number", params.Addresses[i])
+		}
 		c.logsFilters.Addresses[params.Addresses[i]] = struct{}{}
 	}
 
 	for i := range params.Topics {
 		c.logsFilters.Topics[params.Topics[i]] = struct{}{}
 	}
+	return nil
 }
 
 // collects new logs coming from broadcaster and pushes the data into the client response buffer
@@ -83,17 +107,18 @@ func (c *Client) CollectNewLogs() {
 			clientResponse.Method = methodSubscriptionName
 			clientResponse.Params.Subscription = c.newLogsSubscriptionID
 
-			if c.logsFilters != nil {
-				logs, err := c.FilterLogs(newLogs.([]byte))
-				if err != nil {
-					c.log.Error().Err(err).Msg(fmt.Sprintf("filter logs output: %v", err))
-					c.newLogsLocker.Unlock()
-					return
-				} else {
-					clientResponse.Params.Result = logs
-				}
+			logs, err := c.FilterLogs(newLogs.([]byte))
+			if err != nil {
+				c.log.Error().Err(err).Msg(fmt.Sprintf("filter logs output: %v", err))
+				c.newLogsLocker.Unlock()
+				return
+
+			} else if logs == nil {
+				// after filtering we got no logs
+				c.newLogsLocker.Unlock()
+				continue
 			} else {
-				clientResponse.Params.Result = newLogs.([]byte)
+				clientResponse.Params.Result = logs
 			}
 
 			c.newLogsLocker.Unlock()
@@ -113,31 +138,51 @@ func (c *Client) CollectNewLogs() {
 	}
 }
 
-func (c *Client) FilterLogs(newLogs []byte) ([]byte, error) {
-	filteredLogs := make([]source.EthLog, 0)
-	rowLogs := make([]source.EthLog, 0)
-	if err := json.Unmarshal(newLogs, &rowLogs); err != nil {
+// FilterLogs filters logs
+// If no filter is set up, it returns log as it is
+// Otherwise it checks if log contains AT LEAST 1 address from "address" filter OR AT LEAST 1 topic from "topic" filter
+func (c *Client) FilterLogs(newLog []byte) ([]byte, error) {
+	// no filters set up
+	if c.logsFilters == nil {
+		return newLog, nil
+	}
+
+	rowLog := source.EthLog{}
+	if err := json.Unmarshal(newLog, &rowLog); err != nil {
 		return nil, err
 	}
 
 	filters := c.logsFilters
-	for i, rowLog := range rowLogs {
-		// check if log contains address, which is set up as filter
-		_, ok := filters.Addresses[rowLog.Address]
-		if ok {
-			filteredLogs = append(filteredLogs, rowLogs[i])
-		}
 
-		// check if log contains topic, which is set up as filter
-		if rowLog.Topics != nil {
-			for _, topic := range rowLog.Topics {
-				_, ok := filters.Addresses[topic]
-				if ok {
-					filteredLogs = append(filteredLogs, rowLogs[i])
-				}
+	// check if "empty address" filter is set and log contains "0x" address, which is representation for
+	// empty address
+	_, ok1 := filters.Addresses["null"]
+	_, ok2 := filters.Addresses[""]
+	if (ok1 || ok2) && rowLog.Address == "0x" {
+		return newLog, nil
+	}
+
+	// check if log contains address, which is set up as filter
+	_, ok := filters.Addresses[rowLog.Address]
+	if ok {
+		return newLog, nil
+	}
+
+	// check if "empty topic" filter is set and log contains no topics
+	_, ok1 = filters.Topics["null"]
+	_, ok2 = filters.Topics[""]
+	if (ok1 || ok2) && rowLog.Topics == nil {
+		return newLog, nil
+	}
+
+	// check if log contains topic, which is set up as filter
+	if rowLog.Topics != nil {
+		for _, topic := range rowLog.Topics {
+			_, ok := filters.Topics[topic]
+			if ok {
+				return newLog, nil
 			}
 		}
 	}
-
-	return json.Marshal(filteredLogs)
+	return nil, nil
 }
