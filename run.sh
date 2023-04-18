c4f43aa9af87086f73964f2051c59ee6e04d2c29
Rigidus <i.am.rigidus@gmail.com>
2023-04-18 14:52:15 +0300

    run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+export EVM_ADDRESS=eeLSJgWzzxrqKv1UxtRVVH8FX3qCQWUs9QuAjJpETGU
+export NS_COMMUNICATION_CUR_WSSUBSCRIBER_INDEX=0
+export SOLANA_RPC_ENDPOINT=https://api.devnet.solana.com
+go run cmd/neon-wssubscriber/main.go
