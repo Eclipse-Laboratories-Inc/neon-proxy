9205ec5727e3c2464be7fce8cc2eac2ca87110d6
ConvallariaMaj <majalis.conv@gmail.com>
2023-05-29 00:22:17 +0200

    fixed decoder tests

--- internal/indexer/indexed_objects_test.go
+++ internal/indexer/indexed_objects_test.go
@@ -90,30 +90,30 @@ func Test_CompleteEventListValid(t *testing.T) {
 	assert.Equal(t, 2, len(neonTxInfo.neonReceipt.neonTxRes.logs))
 	// test first log
 	log0 := neonTxInfo.neonReceipt.neonTxRes.logs[0]
-	assert.Equal(t, "0x3078313233", log0["address"])
-	assert.Equal(t, "0x3078343536", log0["data"])
-	assert.Equal(t, "0x0", log0["neonEventLevel"])
-	assert.Equal(t, "0x1", log0["neonEventOrder"])
-	assert.Equal(t, "204", log0["neonEventType"])
-	assert.Equal(t, "0x2", log0["neonInnerIxIdx"])
-	assert.Equal(t, true, log0["neonIsHidden"])
-	assert.Equal(t, false, log0["neonIsReverted"])
-	assert.Equal(t, "0x4", log0["neonIxIdx"])
-	assert.Equal(t, "0x:789", log0["neonSolHash"])
+	assert.Equal(t, []string{"0x3078313233"}, log0["address"])
+	assert.Equal(t, []string{"0x3078343536"}, log0["data"])
+	assert.Equal(t, []string{"0x0"}, log0["neonEventLevel"])
+	assert.Equal(t, []string{"0x1"}, log0["neonEventOrder"])
+	assert.Equal(t, []string{"204"}, log0["neonEventType"])
+	assert.Equal(t, []string{"0x2"}, log0["neonInnerIxIdx"])
+	assert.Equal(t, []string{"true"}, log0["neonIsHidden"])
+	assert.Equal(t, []string{"false"}, log0["neonIsReverted"])
+	assert.Equal(t, []string{"0x4"}, log0["neonIxIdx"])
+	assert.Equal(t, []string{"0x:789"}, log0["neonSolHash"])
 	assert.Empty(t, log0["topics"])
 
 	// test second log
 	log1 := neonTxInfo.neonReceipt.neonTxRes.logs[1]
-	assert.Equal(t, "0x30783130313233", log1["address"])
-	assert.Equal(t, "0x30783130343536", log1["data"])
-	assert.Equal(t, "0x1", log1["neonEventLevel"])
-	assert.Equal(t, "0x2", log1["neonEventOrder"])
-	assert.Equal(t, "201", log1["neonEventType"])
-	assert.Equal(t, "0x5", log1["neonInnerIxIdx"])
-	assert.Equal(t, true, log1["neonIsHidden"])
-	assert.Equal(t, true, log1["neonIsReverted"])
-	assert.Equal(t, "0x7", log1["neonIxIdx"])
-	assert.Equal(t, "0x:10789", log1["neonSolHash"])
+	assert.Equal(t, []string{"0x30783130313233"}, log1["address"])
+	assert.Equal(t, []string{"0x30783130343536"}, log1["data"])
+	assert.Equal(t, []string{"0x1"}, log1["neonEventLevel"])
+	assert.Equal(t, []string{"0x2"}, log1["neonEventOrder"])
+	assert.Equal(t, []string{"201"}, log1["neonEventType"])
+	assert.Equal(t, []string{"0x5"}, log1["neonInnerIxIdx"])
+	assert.Equal(t, []string{"true"}, log1["neonIsHidden"])
+	assert.Equal(t, []string{"true"}, log1["neonIsReverted"])
+	assert.Equal(t, []string{"0x7"}, log1["neonIxIdx"])
+	assert.Equal(t, []string{"0x:10789"}, log1["neonSolHash"])
 	assert.Empty(t, log0["topics"])
 }
 
@@ -124,13 +124,13 @@ func Test_NeonIndexedBlockInfoAddTxHolder(t *testing.T) {
 
 	solNeonIx := SolNeonIxReceiptInfo{
 		metaInfo: SolIxMetaInfo{
-			neonTxSig: "0x:123",
+			neonTxSig: "0x123",
 		},
 	}
 
 	neonBlockInfo.AddNeonTxHolder("123", solNeonIx)
 	assert.Equal(t, 1, len(neonBlockInfo.neonHolders))
-	assert.Equal(t, ":123", neonBlockInfo.neonHolders[":123"].key.value)
+	assert.Equal(t, "123:123", neonBlockInfo.neonHolders["123:123"].key.value)
 }
 
 func Test_NeonIndexedBlockInfoAddNeonTx(t *testing.T) {
